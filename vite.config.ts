695f79d71c62319194bea887b27f2cc25581f7d5
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-08-09 11:52:26 -0400

    Trying REST call to HF.

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,7 @@ import { resolve } from 'path'
 export default defineConfig(({ command, mode }) => {
     if (mode != 'lib') {
         return {
-            plugins: [react()],
-            base: '/statosphere'
+            plugins: [react()]
         }
     } else {
         return { plugins: [
