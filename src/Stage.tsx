43e00769dfd5bc9f89989aa6d3f8a836df6c5056
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-08-11 11:33:39 -0400

    Swapping to new backend.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -154,7 +154,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
         );
         const result = await response.json();*/
         console.log(result.data[0]);
-        return result.data[0];
+        return JSON.parse(`${result.data[0]}`);
     }
 
     async beforePrompt(userMessage: Message): Promise<Partial<StageResponse<ChatStateType, MessageStateType>>> {
