5f048a8ecf68f33b1a32a88315589a7067b1ed51
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-08-12 14:51:09 -0400

    Rules draft.

--- src/PromptRule.tsx
+++ src/PromptRule.tsx
@@ -16,6 +16,7 @@ export class PromptRule {
     evaluate(stage: Stage): string {
         if (this.prompt.trim() != '') {
             let expr = Parser.parse(stage.replaceTags(this.condition, {}));
+            console.log(`${stage.replaceTags(this.condition, {})} = ${expr.evaluate()}`);
             return (expr.evaluate() == true ? this.prompt : '');
         } else if (this.subRules.length > 0) {
             return (Object.values(this.subRules).map(rule => rule.evaluate(stage)).filter(retVal => retVal.trim().length > 0).join('\n'))
