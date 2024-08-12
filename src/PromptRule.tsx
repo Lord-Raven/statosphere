b6cfce677b91c7c80890315a565624f3bfe71fdf
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-08-12 14:02:26 -0400

    Rules draft.

--- src/PromptRule.tsx
+++ src/PromptRule.tsx
@@ -1,19 +1,20 @@
 import {Parser} from "expr-eval";
 
 export class PromptRule {
-    rule: string
+    condition: string
     prompt: string
     subRules: PromptRule[]
 
     constructor(data: any) {
-        this.rule = data.rule;
+        console.log(`building rule: ${data.condition}`);
+        this.condition = data.condition;
         this.prompt = data.prompt ?? '';
         this.subRules = data.subRules ?? [];
     }
 
-    evaluate(replace: (input: string, other: any) => string): string {
+    evaluate(replace: (input: string, other: {[key: string]: string}) => string): string {
         if (this.prompt.trim() != '') {
-            return (Parser.evaluate(replace(this.rule, {})) ? this.prompt : '');
+            return (Parser.evaluate(replace(this.condition, {})) ? this.prompt : '');
         } else if (this.subRules.length > 0) {
             return (Object.values(this.subRules).map(rule => rule.evaluate(replace)).filter(retVal => retVal.trim().length > 0).join('\n'))
         }
