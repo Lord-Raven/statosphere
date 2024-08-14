0bf1e8ad75d3159c393247dc1b489cb4fa7d5851
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-08-13 23:23:07 -0400

    Cleaning up.

--- src/PromptRule.tsx
+++ src/PromptRule.tsx
@@ -7,7 +7,6 @@ export class PromptRule {
     subRules: PromptRule[]
 
     constructor(data: any) {
-        console.log(`building rule: ${data.condition}`);
         this.condition = data.condition;
         this.prompt = data.prompt ?? '';
         this.subRules = data.subRules ?? [];
@@ -15,8 +14,6 @@ export class PromptRule {
 
     evaluate(stage: Stage): string {
         if (this.prompt.trim() != '') {
-            //console.log(`Condition: ${this.condition.toLowerCase()}`);
-            //console.log(`${stage.replaceTags(this.condition.toLowerCase(), {})} = ${Parser.evaluate(stage.replaceTags(this.condition.toLowerCase(), {}))}`);
             return (Parser.evaluate(stage.replaceTags(this.condition.toLowerCase(), {})) ? this.prompt : '');
         } else if (this.subRules.length > 0) {
             return (Object.values(this.subRules).map(rule => rule.evaluate(stage)).filter(retVal => retVal.trim().length > 0).join('\n'))
