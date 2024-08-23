349c847826cee7dab7a93dbe34cf7683ae08f655
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-08-23 17:14:01 -0400

    Modifications to new evaluation and schema setup.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -278,8 +278,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
         }
         replacements['content'] = this.content;
         return source.replace(/{{([A-z]*)}}/g, (match) => {
-            let replacement: string = replacements[match.substring(2, match.length - 2).toLowerCase()];
-            return replacement ? replacement.replace(/"/g, '\\"') : replacement;
+            return String(replacements[match.substring(2, match.length - 2).toLowerCase()]).replace(/"/g, '\\"');
         });
     }
 
