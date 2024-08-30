18b0b27a1306a600cec7b167309cf5d274dbe8dc
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-08-30 17:04:25 -0400

    Debugging function support.

--- src/Variable.tsx
+++ src/Variable.tsx
@@ -26,5 +26,6 @@ export class Variable {
         this.definitionName = definitionName;
         console.log(variableDefinitions[definitionName].initialValue);
         this.value = stage.evaluate(stage.replaceTags(variableDefinitions[definitionName].initialValue, {}));
+        console.log(this.value);
     }
 }
