fedb80506f2bc82a068f0cd4f848e9e51ad33f62
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-08-30 22:28:20 -0400

    Debugging function support.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -121,9 +121,10 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
         console.log('Validate functions');
         Object.values(this.validateSchema(this.config.functionConfig ?? data.config_schema.properties.functionConfig.value, functionSchema, 'function schema'))
             .forEach(funcData => {let customFunction = new CustomFunction(funcData); this.functions[customFunction.name] = customFunction.createFunction()});
-        Object.entries(this.functions).forEach(([key, value]) => {
-            this.customFunctionMap[`${key}`] = factory(key, [], () => function testFunction(): any {return 1;});
-        });
+        //Object.entries(this.functions).forEach(([key, value]) => {
+        //    this.customFunctionMap[`${key}`] = factory(key, [], () => function testFunction(): any {return 1;});
+        //});
+        this.customFunctionMap[`testFunction`] = factory('testFunction', [], () => function testFunction(): number {return 1;});
         console.log(this.customFunctionMap);
         this.evaluate = create(this.customFunctionMap, {matrix: 'Array'}).evaluate;
 
