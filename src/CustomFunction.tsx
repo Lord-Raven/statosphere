9ea7756ca06421b9a1e0f36a65cc84c21f970b10
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-09-06 21:40:16 -0400

    Adding post input system message support.

--- src/CustomFunction.tsx
+++ src/CustomFunction.tsx
@@ -16,8 +16,6 @@ export class CustomFunction {
     createFunction() {
         let finalParameters = [...(this.parameters ? this.parameters.split(',').filter(item => item).map(item => item.trim()) : []),
             ...(this.dependencies ? this.dependencies.split(',').filter(item => item).map(item => item.trim()) : [])];
-        console.log(finalParameters);
-        console.log(this.body);
         if (finalParameters.length > 0) {
             return new Function(...finalParameters, this.body);
         } else {
