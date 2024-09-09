a81ba2e1abf50e446b669c3e7f36e390c959af61
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-09-08 20:29:22 -0400

    Looking at possible stage direction issue.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -412,7 +412,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
 
         console.log('End beforePrompt()');
         return {
-            stageDirections: stageDirections.trim() != '' ? `[INST]${stageDirections}\n[/INST]` : null,
+            stageDirections: stageDirections.trim() != '' ? `[RESPONSE INSTRUCTION]${stageDirections}\n[/RESPONSE INSTRUCTION]` : null,
             messageState: this.writeMessageState(),
             modifiedMessage: modifiedMessage,
             systemMessage: systemMessage.trim() != '' ? systemMessage : null,
