f3c31002cc1d2feb6fe030ab30a2283bbd507bf9
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-09-30 10:58:12 -0400

    Drafting a text generation feature.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -378,7 +378,9 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
     kickOffGenerators(phase: Phase): {[key: string]: Promise<TextResponse|null>} {
         let promises: {[key: string]: Promise<TextResponse|null>} = {};
         for (const generator of Object.values(this.generators)) {
+            console.log(`Consider generator of phase ${generator.phase}: ${generator.phase == phase} && ${generator.condition} != '' && ${this.evaluate(this.replaceTags(generator.condition ?? 'true'), this. buildScope())}`);
             if (generator.phase == phase && (generator.condition != '' && !this.evaluate(this.replaceTags(generator.condition ?? 'true'), this.buildScope()))) {
+                console.log('Kicking off a generator');
                 promises[generator.name] = this.generator.textGen({prompt: generator.prompt, min_tokens: generator.minTokens, max_tokens: generator.maxTokens});
             }
         }
