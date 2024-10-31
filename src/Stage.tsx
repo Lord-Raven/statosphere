cca3f560554b7703c3e00740f445de6166db1c04
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-10-31 08:44:06 -0400

    Debug messaging to help diagnose a potential function dependency issue.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -155,7 +155,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
         Object.values(this.functions).forEach(thisFunction => {
             thisFunction.body = this.updateFunctionArguments(thisFunction.body);
             try {
-                console.log(`Built function ${thisFunction.name}(${thisFunction.parameters}, ${thisFunction.dependencies}) {\n${thisFunction.body}\n}`);
+                console.log(`Built function ${thisFunction.name}(${thisFunction.parameters}${thisFunction.dependencies}) {\n${thisFunction.body}\n}`);
                 this.customFunctionMap[`${thisFunction.name}`] = thisFunction.createFunction();
             } catch (error) {
                 console.log(error);
@@ -166,6 +166,10 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
         });
 
         math.import(this.customFunctionMap);
+        for(let name in Object.keys(this.customFunctionMap)) {
+            console.log(`Checking whether ${name} was imported into mathJS: ${math.isFunction(name)}`);
+        }
+
         this.evaluate = math.evaluate;
         //this.evaluate = create(this.customFunctionMap, {matrix: 'Array'}).evaluate;
 
