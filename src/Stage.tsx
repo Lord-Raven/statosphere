59976bd38440af53e415c772dce1ef817a8e60fb
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2025-04-23 20:55:51 -0400

    Addressing a potential NPE.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -614,7 +614,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
     }
 
     replaceTags(source: string) {
-        if (!source) return source;
+        if (!source) return '';
         let replacements = this.replacements;
         for (const key of Object.keys(this.variables)) {
             replacements[key.toLowerCase()] = (typeof this.getVariable(key) in ['object','string'] ? JSON.stringify(this.getVariable(key)) : this.getVariable(key));
