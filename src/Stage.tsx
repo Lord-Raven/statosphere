56036fe100c77a9d056ef13420dd9e1ae165597e
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2025-06-14 16:18:04 -0400

    Adding a built-in split() method.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -121,7 +121,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
         console.log('Validate functions');
         // Build basic functions
         this.functions = {
-            split: new CustomFunction({name: 'split', parameters: 'haystack needle', body: `\
+            split: new CustomFunction({name: 'split', parameters: 'haystack, needle', body: `\
                         return haystack.split(needle);`
             }, this),
             contains: new CustomFunction({name: 'contains', parameters: 'haystack, needle', body: `\
